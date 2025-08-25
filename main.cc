07259fecfbbd5ee0940797e7a9b2e5a6787f681d
aphd <aphidarium@proton.me>
2025-08-25 15:35:48 +0100

    hello triangle

--- main.cc
+++ main.cc
@@ -33,15 +33,85 @@ int main() {
   glfwMakeContextCurrent(window);
   gladLoadGL();
 
+  float vertices[] = {
+       0.0f,  0.5f,
+       0.5f, -0.5f,
+      -0.5f, -0.5f
+  };
+
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  const char* vertexSource = R"glsl(
+    #version 150 core
+
+    in vec2 position;
+
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+  )glsl";
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexSource, NULL);
+  glCompileShader(vertexShader);
+
+  const char* fragmentSource = R"glsl(
+    #version 150 core
+
+    out vec4 fragColor;
+
+    void main()
+    {
+        fragColor = vec4(1.0);
+    }
+  )glsl";
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+  glCompileShader(fragmentShader);
+
+  GLint status;
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+  if (!status) {
+    std::cerr << "BAD\n";
+  }
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+  if (!status) {
+    std::cerr << "BAD\n";
+  }
+  std::cout << glGetError();
+
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glBindFragDataLocation(shaderProgram, 0, "fragColor");
+  glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
+
+  GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+  glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(posAttrib);
+
   while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
+    glClear(GL_COLOR_BUFFER_BIT);
 
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
-    glClear(GL_COLOR_BUFFER_BIT);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
+    glfwPollEvents();
   }
 
   glfwDestroyWindow(window);
